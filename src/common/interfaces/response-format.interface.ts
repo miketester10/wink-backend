6101d7fcb7497cceb850e09224625cb5b28d05b8
miketester10 <cadenza91@gmail.com>
2026-03-05 20:27:54 +0100

    refactor: remove default string type from ResponseBase generic parameter.

--- src/common/interfaces/response-format.interface.ts
+++ src/common/interfaces/response-format.interface.ts
@@ -1,13 +1,13 @@
 import { HttpExceptionBodyMessage } from '@nestjs/common';
 
-interface ResponseBase<T = string> {
+interface ResponseBase<T> {
   message: T;
   statusCode: number;
 }
 
 // Success → message sempre string
 export interface ResponseSuccess<T = unknown> extends ResponseBase<string> {
-  data?: T; // può essere undefined perché non tutti gli endpoint ritornano dati
+  data?: T; // può essere opzionale perché non tutti gli endpoint sono obbligati a ritornare dati
 }
 
 // Error → message può essere string | string[] | number
